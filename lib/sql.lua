d8ce375413cd75623ff1cb658d1fbe7742fd96d3
Pavel Vomacka <pvomacka@redhat.com>
2015-09-10 14:32:02 +0200

    Added function for getting last insert row id.

--- lib/sql.lua
+++ lib/sql.lua
@@ -26,7 +26,7 @@ sql.__index = sql
 --  @return sqlite3 object.
 function sql.create(fileName)
     if not fileName then
-        fail("File name has to be set.")
+        fail("sql.lua: File name has to be set.")
         return nil
     end
 
@@ -38,7 +38,7 @@ function sql.create(fileName)
 
     -- Check whether file exists.
     if not path.file_exists(s.file) then
-        fail("Database file does not exist.")
+        fail("sql.lua: Database file does not exist.")
         return nil
     end
 
@@ -64,22 +64,39 @@ end
 
 
 --
+--- Function that find id of last inserted row.
+--
+--  @return last insert id as string
+function sql:lastInsertedRowId()
+    -- Compose command for getting last inserted row.
+    local command = "sqlite3 \"" .. self.file .. "\" \".last_insert_rowid()"
+
+    -- Execute command.
+    local output = execCaptureOutputAsString(command)
+
+    -- Return the output.
+    return output
+end
+
+
+--
 --- Execute slq query.
+--  Use apostrophes to quoting string in queries which will be executed by this function.
 --
 --  @param query which will be executed
 --  @return table with the output of the query,
 function sql:executeQueryGetAll(query)
     -- Compose commanf for executing query
+
     local command = "sqlite3 \"" .. self.file .. "\" \"" .. query .. "\""
 
     -- Execute command.
     local output = execCaptureOutputAsTable(command)
-
     -- Return nil when table is empty.
     if table.isEmpty(output) then
         return nil
     end
-    
+
     -- Return the output.
     return output
 end
@@ -87,11 +104,12 @@ end
 
 --
 --- Execute slq query (and return only first result).
+--  Use apostrophes to quoting string in queries which will be executed by this function.
 --
 --  @param query which will be executed
 --  @return the first line from output,
 function sql:executeQueryGetFirst(query)
-    -- Compose commanf for executing query
+    -- Compose command for executing query
     local command = "sqlite3 \"" .. self.file .. "\" \"" .. query .. "\""
 
     -- Execute command.
