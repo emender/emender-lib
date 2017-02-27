406125eb4f3c6f83934e01825d05a7cd25124932
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-02-27 15:25:13 +0100

    Test for docbook.readDocbookVersion()

---
+++ test/testDocbook.lua
@@ -0,0 +1,41 @@
+package.path=package.path..";../lib/?.lua"
+
+function fail(message)
+    print(message)
+end
+
+local ok = true
+
+require("docbook")
+
+function checkVersion(filename, expectedVersion)
+    local version = docbook.readDocbookVersion(filename)
+    if not version then
+        print("failure: version is not recognized")
+        ok = false
+    elseif version == expectedVersion then
+        print("ok " .. version)
+    else
+        print("failure " .. expectedVersion .. " != " .. version) 
+        ok = false
+    end
+end
+
+checkVersion("Test_Book_4_0.xml", "4.0")
+checkVersion("Test_Book_4_2.xml", "4.2")
+checkVersion("Test_Book_4_3.xml", "4.3")
+checkVersion("Test_Book_4_4.xml", "4.4")
+checkVersion("Test_Book_4_5.xml", "4.5")
+
+checkVersion("Test_Book_4_0_b.xml", "4.0")
+checkVersion("Test_Book_4_2_b.xml", "4.2")
+checkVersion("Test_Book_4_3_b.xml", "4.3")
+checkVersion("Test_Book_4_4_b.xml", "4.4")
+checkVersion("Test_Book_4_5_b.xml", "4.5")
+
+checkVersion("Test_Book_5_0.xml", "5.0")
+
+if not ok then
+    os.exit(1)
+end
+
